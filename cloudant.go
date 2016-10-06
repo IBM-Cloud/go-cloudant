d2464d02d213754648c865a7b0888edc79ccb791
Yun Wang <wangyun@us.ibm.com>
2016-10-06 11:12:20 -0400

    Add wrapper for EnsureDB

--- cloudant.go
+++ cloudant.go
@@ -77,6 +77,17 @@ func (c *Client) CreateDB(dbName string) (*DB, error) {
 	return &DB{db, c.username, c.password, dbPath}, nil
 }
 
+// EnsureDB ensures that a database with the given name exists.
+func (c *Client) EnsureDB(name string) (*DB, error) {
+	var db *couchdb.DB
+	var err error
+	if db, err = c.Client.EnsureDB(name); err != nil {
+		return nil, err
+	}
+	dbPath := c.Client.URL() + "/" + name
+	return &DB{db, c.username, c.password, dbPath}, nil
+}
+
 // DeleteDB ...
 func (c *Client) DeleteDB(dbName string) error {
 	return c.Client.DeleteDB(dbName)
